29c725dc0987f86446c49b9cc62f5e53051875ab
thaiduong1011 <36438062+thaiduong1011@users.noreply.github.com>
2018-04-14 13:01:23 +0700

    Add code to new branch

---
+++ app/src/main/java/android/dkh/com/housingapp/MenuActivity.java
@@ -0,0 +1,13 @@
+package android.dkh.com.housingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
